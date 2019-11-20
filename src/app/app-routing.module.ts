43c4f5d4cd4ca0f4b607d2e2d1685b25f02b21e7
Pablo Romero Montero <pabloromeromontero23@gmail.com>
2019-11-20 02:26:30 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {PeliculaComponent} from './components/pelicula/pelicula.component';
+import {LocalizarComponent} from './components/localizar/localizar.component';
+import {LoginComponent} from './components/login/login.component';
+import {BuscarComponent} from './components/buscar/buscar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent, canActivate: []},
+  {path: 'pelicula/:id', component: PeliculaComponent, canActivate: []},
+  {path: 'localizar/:id', component: LocalizarComponent, canActivate: []},
+  {path: 'login', component: LoginComponent},
+  {path: 'buscar/:id', component: BuscarComponent, canActivate: []},
+  {path: 'buscar', component: BuscarComponent, canActivate: []},
+  {path: '**', component: HomeComponent, canActivate: []}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PeliculaComponent } from './components/pelicula/pelicula.component';
+
+import { LocalizarComponent } from './components/localizar/localizar.component';
+import { BuscarComponent } from './components/buscar/buscar.component';
+import { HeaderComponent } from './components/header/header.component';
+import {LoginComponent} from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PeliculaComponent,
+    LoginComponent,
+    LocalizarComponent,
+    BuscarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
